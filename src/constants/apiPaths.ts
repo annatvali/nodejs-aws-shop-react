4ff8999fbdd3b146f1214b61280678b3ea178a45
annatvali <anntvaliashvili@gmail.com>
2024-07-02 01:12:32 +0400

    refactor: update API_PATHS for import endpoint

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://p62pvbxud2.execute-api.eu-central-1.amazonaws.com/prod/",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://cmn9rzw83j.execute-api.eu-central-1.amazonaws.com/prod/",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
