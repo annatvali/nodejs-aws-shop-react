f2c9856b42df2f498611659457182b678001d90e
annatvali <anntvaliashvili@gmail.com>
2024-07-18 03:59:24 +0400

    feat: implement Basic Auth for file uploads by retrieving token from localStorage

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://p62pvbxud2.execute-api.eu-central-1.amazonaws.com/prod",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://cmn9rzw83j.execute-api.eu-central-1.amazonaws.com/prod",
+  import: "https://d1ozlk0djyt1ga.execute-api.eu-central-1.amazonaws.com/prod",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
